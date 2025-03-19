32af2841716798f5f5da9e94ce4fff150ecf56cd
Malcolm Steen <msteen@LFPN-Blenheim.lfpn.ds.mpg.de>
2025-03-19 18:19:32 +0100

    Change variable names

--- tests/test_n_comp_sim.py
+++ tests/test_n_comp_sim.py
@@ -24,8 +24,8 @@ def test_get_state_init(my_simulator: NCompSimulator):
 
     state_init = my_simulator.get_state_init(0)
     assert state_init.shape == (2, my_simulator.N)
-    assert state_init[0, 0] == my_simulator.phi_d_dil_spin[0] * 0.97
-    assert state_init[0, -1] == my_simulator.phi_d_den_spin[0] * 1.03
+    assert state_init[0, 0] == my_simulator.phi_d_dil_spin[0]
+    assert state_init[0, -1] == my_simulator.phi_d_den_spin[0]
     assert np.all(state_init[1] == my_simulator.phi_r_spin[0])
 
     my_simulator.is_cp_data = True
@@ -35,7 +35,7 @@ def test_get_state_init(my_simulator: NCompSimulator):
 
 
 def test_calc_spinodal(my_simulator: NCompSimulator):
-    phi_r, phi_d_dil, phi_d_den = my_simulator.calc_spinodal()
+    phi_d_den, phi_d_dil, phi_r = my_simulator.calc_spinodal()
     assert phi_r.shape == phi_d_den.shape == phi_d_dil.shape
     assert np.all(phi_d_den > phi_d_dil)
     assert np.all(phi_d_dil > 0)
@@ -43,14 +43,4 @@ def test_calc_spinodal(my_simulator: NCompSimulator):
     assert np.diff(phi_r).min() > 0
 
 
-import matplotlib.pyplot as plt
-
-chi_ds = 2.5
-for chi_dr in np.linspace(0.01, 1.5, 3):
-    sim = NCompSimulator(chi_dr, chi_ds, "")
-    phi_r, phi_d_dil, phi_d_den = sim.calc_spinodal()
-
-    plt.scatter(phi_r, phi_d_dil, color="red")
-    plt.scatter(phi_r, phi_d_den, color="blue")
-
-plt.show()
+# def test_calc_binodal(my_simulator: NCompSimulator):
